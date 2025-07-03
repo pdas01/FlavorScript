23ea2f43d41d5d1259dfd31afbdc331ef8ca1bc5
pdas01 <34662405+pdas01@users.noreply.github.com>
2025-07-03 10:22:34 -0700

    Update YoutubrPlayer

--- RecipeApp/RecipeApp/UI/YouTubePlayerView.swift
+++ RecipeApp/RecipeApp/UI/YouTubePlayerView.swift
@@ -4,6 +4,7 @@ import WebKit
 struct YouTubePlayerView: UIViewRepresentable {
     private let url: URL
 
+
     init?(videoID: String) {
         guard let url = URL(string: "https://www.youtube.com/embed/\(videoID)") else { return nil }
         self.init(url: url)
@@ -14,13 +15,37 @@ struct YouTubePlayerView: UIViewRepresentable {
     }
 
     func makeUIView(context: Context) -> WKWebView {
+        // Potential memory leak here WKWebView retains itself  to its internal use of delegate and observer patterns. Need to up its navigation delegate or stop loading, it may linger in memory.
         let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
         return webView
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        uiView.load(request)
+        // Load only when needed
+        if uiView.url != url {
+            uiView.stopLoading()
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
     }
+    
+    // MARK: - Coordinator
+
+       func makeCoordinator() -> Coordinator {
+           Coordinator()
+       }
+
+       class Coordinator: NSObject, WKNavigationDelegate {
+           deinit {
+               print("Coordinator deinitialized")
+           }
+
+           func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+               print("WebView finished loading")
+           }
+
+           // Add error handling, loading progress, etc.
+       }
 }
 
